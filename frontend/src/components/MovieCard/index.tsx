0f3dc7a510a5ca106168bf20c663c551510bd3d4
vagnyrbr <vagnyr79@gmail.com>
2022-01-17 00:01:09 -0300

    Ajuste erro netlify

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,5 +1,4 @@
 import MovieScore from "components/MovieScore";
-import { type } from "os";
 import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
@@ -11,12 +10,12 @@ function MovieCard( { movie } : Props) {
 
     return (
         <div>
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <img className="dsmovie-movie-card-image" src={movie?.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
-                <h3>{movie.title}</h3>
-                <MovieScore count={movie.count} score={movie.score} />
+                <h3>{movie?.title}</h3>
+                <MovieScore count={movie?.count} score={movie?.score} />
 
-                <Link to={`/form/${movie.id}`}>
+                <Link to={`/form/${movie?.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
 
